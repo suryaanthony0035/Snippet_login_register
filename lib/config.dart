00090bc486234ef8949b56581baf93634bd0accb
suryaanthony0035 <exdrs.emp.0035.surya@gmail.com>
2022-05-28 11:11:17 +0530

    done is profile page

--- lib/config.dart
+++ lib/config.dart
@@ -4,4 +4,6 @@ class Config {
   static const String loginAPI = "/api/auth/login";
   static const String registerAPI = "/api/auth/register";
   static const userProfileAPI = "/api/user/myAccount/";
+  static const getProduct = "/api/product";
+  static const getImageUrl = "http://mvs.bslmeiyu.com/uploads/";
 }
